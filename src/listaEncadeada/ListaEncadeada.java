b90baca4414c5b9003a6a7907573053444237b28
Carolina Praxedes <ana_carolina47@hotmail.com>
2022-11-09 22:03:03 -0300

    Precisa corrigir a lista encadeada

--- src/listaEncadeada/ListaEncadeada.java
+++ src/listaEncadeada/ListaEncadeada.java
@@ -46,9 +46,62 @@ public class ListaEncadeada {
 		}
 		this.tamanho++;
 	}
-	public void remover(Object novoElemento) {
+	//Esse código serve para usar em pilha
+	/*public void adicionarInicio(Object novoElemento) {
+		//para criar um novo é preciso criar um novo novo
+		No novoNo = new No(novoElemento);
+		//pasando o valor para o no
+		if(this.primeiro==null && this.ultimo==null) {
+			this.primeiro = novoNo;
+			this.ultimo = novoNo;
 		
+		}else {
+			novoNo.setNext(primeiro);
+			//estou dizendo que o proximo elemento vai ser o primeiro
+			this.primeiro=novoNo;
+			//colocando o valor no primeiro nó 
+			
+		}
+		this.tamanho++;
+	}*/
+	
+	public void remover(Object elementoProcurado) {
+		
+		No atual = this.primeiro;
+		No anterior = null; //é necessário uma variavel para guardar o anterior
+		for(int i=0;i<this.getTamanho();i++) {
+			if(atual.getElement()== elementoProcurado) {//vou verificar se o valor do atual é oq estamos procurando
+				if(this.getTamanho()==1) {//quando quiser zerar a lista, ele entra ai
+					this.primeiro=null;
+					this.ultimo=null;
+				}else if(atual==primeiro) {//RETIRAR O PRIMEIRO NO
+					this.primeiro= atual.getNext();//agr o primeiro vai ser o proximo do atual
+					atual.setNext(null);//removendo o primeiro novo
+					
+				}else if(atual==ultimo){ //Retira o ultimo novo
+					this.ultimo = anterior; //o ultimo vai ser o anterior do no atual
+					anterior.setElement(null);
+				}else { //Retira um nó entre o primeiro e o ultimo
+					anterior.setNext(atual.getNext());//o proximo no do anterior vai ser o proximo no do atual
+					
+					atual=null;
+				}
+				
+				this.tamanho--;
+				break;
+			}
+			anterior = atual;//eu estou guardando o atual antes que ele ande no loop
+			atual = atual.getNext();
+			//o atual esta andando junto com i no loop
+		}
 	}
+	
+	/*public void removerPrimeiro() {
+		No atual = primeiro;
+		atual.setNext(atual);
+			
+	}*/
+	
 	public No get(int posicao) {
 		//é um metodo para visualizar todos da lista e encontrar o elemento que foi repassado
 		No atual = this.primeiro;
@@ -66,5 +119,11 @@ public class ListaEncadeada {
 	@Override
 	public String toString() {
 		return this.primeiro.toString();
+	}
+	
+	public void showLista() {
+		for(int i=0;i<getTamanho();i++) {
+			System.out.println(get(i).getElement());
+		}
 	}
 }
--- src/listaEncadeada/ListaEncadeadaTeste.java
+++ src/listaEncadeada/ListaEncadeadaTeste.java
@@ -7,13 +7,22 @@ public class ListaEncadeadaTeste {
 		lista.adicionar("ac");
 		lista.adicionar("BA");
 		lista.adicionar("CE");
+
+		
 		System.out.println("Tamanho: "+lista.getTamanho());
 		System.out.println("Primeiro: "+lista.getPrimeiro().getElement());
 		System.out.println("Ultimo: "+lista.getUltimo().getElement());
-		
-		System.out.println(lista.get(0).getElement());
-		System.out.println(lista.get(1).getElement());
-		System.out.println(lista.get(2).getElement());
+		System.out.println("=======================================");
+		lista.showLista();
+	
+		//System.out.println(lista.get(3).getElement());
+		System.out.println("===========================");
+		lista.remover("ac");
+		lista.showLista();
+		lista.adicionar("SP");
+		lista.showLista();
+		System.out.println("Tamanho: "+lista.getTamanho());
+
 		
 
 	}
